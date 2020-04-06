197a013383c463d4dce87fd8adeea2160631dd9f
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-05 19:55:47 -0500

    Convert the script to Typescript

--- src/index.ts
+++ src/index.ts
@@ -22,13 +22,12 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ******************************************************************************* */
 
-/* eslint-disable max-len */
-/* eslint-disable no-console */
-
 /**
  * Has a set of static functions that allows to save or load data from localStorage or sessionStorage.
  */
 export default class Storage {
+  errorMessage: string;
+
   constructor() {
     this.errorMessage = 'ERROR: No web storage support. Using a temporal storage instead';
   }
@@ -38,12 +37,10 @@ export default class Storage {
    * @param {String} storage The name of the storage in localStorage that will be loaded.
    * @param {String} oldStorage (Optional) The name of the storage in localStorage that will be deleted.
    */
-  static localLoad(storage, oldStorage = null) {
-    if (oldStorage !== null && localStorage.getItem(oldStorage)) {
-      localStorage.removeItem(oldStorage);
-    }
-    if (localStorage.getItem(storage)) return JSON.parse(localStorage.getItem(storage));
-    return null;
+  localLoad(storage: string, oldStorage: string = '') {
+    if (oldStorage !== '' && localStorage.getItem(oldStorage)) localStorage.removeItem(oldStorage);
+    const parsedData: string | null = localStorage.getItem(storage);
+    return parsedData ? JSON.parse(parsedData) : null;
   }
 
   /**
@@ -51,12 +48,10 @@ export default class Storage {
    * @param {String} storage The name of the storage in sessionStorage that will be loaded.
    * @param {String} oldStorage (Optional) The name of the storage in sessionStorage that will be deleted.
    */
-  static sessionLoad(storage, oldStorage = null) {
-    if (oldStorage !== null && sessionStorage.getItem(oldStorage)) {
-      sessionStorage.removeItem(oldStorage);
-    }
-    if (sessionStorage.getItem(storage)) return JSON.parse(sessionStorage.getItem(storage));
-    return null;
+  sessionLoad(storage: string, oldStorage: string = '') {
+    if (oldStorage !== null && sessionStorage.getItem(oldStorage)) sessionStorage.removeItem(oldStorage);
+    const parsedData: string | null = sessionStorage.getItem(storage);
+    return parsedData ? JSON.parse(parsedData) : null;
   }
 
   /**
@@ -64,12 +59,12 @@ export default class Storage {
    * @param {String} storage the name of the storage.
    * @param {Object} data the object that will be saved in the storage.
    */
-  static localSave(storage, data) {
+  localSave(storage: string, data: object) {
     if (typeof (Storage) !== 'undefined') {
       localStorage.setItem(storage, JSON.stringify(data));
       return true;
     }
-    console.log(this.errorMessage);
+    alert(this.errorMessage);
     return false;
   }
 
@@ -78,12 +73,12 @@ export default class Storage {
    * @param {String} storage the name of the storage.
    * @param {Object} data the object that will be saved in the storage.
    */
-  static sessionSave(storage, data) {
+  sessionSave(storage: string, data: object) {
     if (typeof (Storage) !== 'undefined') {
       sessionStorage.setItem(storage, JSON.stringify(data));
       return true;
     }
-    console.log(this.errorMessage);
+    alert(this.errorMessage);
     return false;
   }
 }
